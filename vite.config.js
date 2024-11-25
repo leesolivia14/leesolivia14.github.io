985ed9ded8829729f0986d0a7d46b8b451321009
leesolivia14 <sl8055@nyu.edu>
2024-11-25 00:39:19 -0800

    Updates

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/leesolivia14.github.io/",
+  build: {
+    outDir: 'dist',  // This specifies that the output directory will be 'dist'
+  },
+  base: "/",
 })
